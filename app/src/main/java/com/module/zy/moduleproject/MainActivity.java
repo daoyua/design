b90a1cbb1bb75a386cf05c6891005c62a51eae4c
Zhou, Yang (133-Extern) <futuremove.zhou@daimler.com>
2018-08-22 16:13:28 +0800

    something

--- app/src/main/java/com/module/zy/moduleproject/MainActivity.java
+++ app/src/main/java/com/module/zy/moduleproject/MainActivity.java
@@ -1,15 +1,32 @@
 package com.module.zy.moduleproject;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View view = this.getLayoutInflater().inflate(R.layout.snackbar_layout, null);
+        FrameLayout test_f = findViewById(R.id.test_f);
+        Snackbar.make(test_f, "hello world", Snackbar.LENGTH_LONG).show();
+
+        initview();
+    }
+
+    private void initview() {
+        toolbar1 = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar1);
+
     }
 }
