b6ac7d69b9bbc706424091331ad459a8f5dee264
Zhou, Yang (133-Extern) <futuremove.zhou@daimler.com>
2018-08-22 18:01:35 +0800

    something

--- app/src/main/java/com/module/zy/moduleproject/MainActivity.java
+++ app/src/main/java/com/module/zy/moduleproject/MainActivity.java
@@ -3,30 +3,42 @@ package com.module.zy.moduleproject;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar1;
+    private CoordinatorLayout rootlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        View view = this.getLayoutInflater().inflate(R.layout.snackbar_layout, null);
-        FrameLayout test_f = findViewById(R.id.test_f);
-        Snackbar.make(test_f, "hello world", Snackbar.LENGTH_LONG).show();
 
         initview();
     }
 
     private void initview() {
+        rootlayout = findViewById(R.id.rootlayout);
+        Snackbar.make(rootlayout,"helloword",Snackbar.LENGTH_SHORT).show();
+
         toolbar1 = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar1);
 
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("tab1"));
+        tabLayout.addTab(tabLayout.newTab().setText("tab2"));
+        tabLayout.addTab(tabLayout.newTab().setText("tab3"));
+        tabLayout.setupWithViewPager(new ViewPager(this));
     }
 }
