9cb3a45bbeafcabc19ab946e09aed602461862e9
zhouyang <814575934@qq.com>
2018-09-29 23:04:22 +0800

    mvp搭建完毕

--- app/src/main/java/com/module/zy/moduleproject/Fragment1/dataResponse/BaseResponse.java
+++ app/src/main/java/com/module/zy/moduleproject/Fragment1/dataResponse/BaseResponse.java
@@ -1,12 +1,14 @@
 package com.module.zy.moduleproject.Fragment1.dataResponse;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 public class BaseResponse implements Serializable {
-//    @SerializedName("time_stamp")
-//    public String time_stamp;
-//    @SerializedName("msg")
-//    public String msg;
-//    @SerializedName("status")
-//    public String status;
+    @SerializedName("time_stamp")
+    public String time_stamp;
+    @SerializedName("msg")
+    public String msg;
+    @SerializedName("status")
+    public String status;
 }
