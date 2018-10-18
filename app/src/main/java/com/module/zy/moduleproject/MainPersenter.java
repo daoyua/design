50611def090e3ed17cb7695f3afad83a21c09ed9
zhouyang <814575934@qq.com>
2018-10-18 23:28:00 +0800

    写了点

--- app/src/main/java/com/module/zy/moduleproject/MainPersenter.java
+++ app/src/main/java/com/module/zy/moduleproject/MainPersenter.java
@@ -6,7 +6,10 @@ import com.module.zy.moduleproject.Fragment1.view.MainView;
 import com.module.zy.moduleproject.response.UserResponse;
 import com.module.zy.moduleproject.retrofit2.MynetworkManager;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Predicate;
 import module.base.baseframwork.base.presenter.BasePresenter;
 import module.base.baseframwork.base.retrofit.CompositeDisposableInter;
 import module.base.baseframwork.base.retrofit.SimpleSubscriber;
@@ -31,7 +34,13 @@ public class MainPersenter extends BasePresenter<MainView> {
     public void getdata() {
 
         // 3 创建接口的代理对象
-      MynetworkManager.getData("绵阳市")
+      MynetworkManager.getData("绵阳市").filter(new Predicate<UserResponse>() {
+          @Override
+          public boolean test(UserResponse userResponse) throws Exception {
+              return userResponse.getCityName().length()>2;//过滤长度大于2的才通过
+          }
+      }).throttleFirst(2,TimeUnit.SECONDS)//只有第一次点击有效
+              .debounce(1000, TimeUnit.SECONDS)
                 .subscribe(new SimpleSubscriber<UserResponse>() {
                     @Override
                     public CompositeDisposableInter initCompositeDisposableInter() {
@@ -39,6 +48,7 @@ public class MainPersenter extends BasePresenter<MainView> {
                             @Override
                             public void setDidposable(Disposable disposable) {
                                 myCompositeDisposable.add(disposable);
+
                             }
                         };
                     }
